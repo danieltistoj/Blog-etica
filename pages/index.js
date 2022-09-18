505017b3c81af9eeba4aa2e5b28052c93e49cc76
elmergustavo <elmergustavo79@gmail.com>
2022-09-18 17:47:40 -0600

    cards add

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import Search from "../components/Search";
 import styles from "../styles/Home.module.css";
+import Imagen from "../public/img/miami.jpg";
 
 export default function Home() {
   const [searchText, setSearchText] = useState("");
@@ -12,9 +13,9 @@ export default function Home() {
     setSearchText(v);
   };
   return (
-    <main className="bg-darkbg h-screen">
+    <main className="bg-darkbg h-full">
       <div className="text-white px-8 md:p-8 flex flex-col md:block items-start justify-start mx-auto w-full md:w-[70%]">
-        <div className="h-screen md:h-fit md:block flex flex-col items-center justify-center">
+        <div className=" md:h-fit md:block flex flex-col items-center justify-center">
           <div className="text-5xl md:text-6xl font-semibold bg-gradient-to-r bg-clip-text font-Work text-transparent from-rose-100 via-indigo-500 to-teal-100 animate-text">
             Blog
           </div>
@@ -30,6 +31,138 @@ export default function Home() {
           <Search value={searchText} setter={onChangeSearch} />
           <div className="space-y-8 mt-12"></div>
         </div>
+        <section className="grid grid-cols-1 md:grid-cols-4 space-x-5">
+          <div className="flex flex-col justify-center">
+            <div className="w-auto rounded-2xl">
+              <Image src={Imagen} alt="" className="rounded-2xl" />
+            </div>
+            <p className="font-bold spa my-2">Etica en la ingenieria</p>
+
+            <p className="my-2  text-xs flex items-center justify-center">18 de septiembre del 2022</p>
+
+            <p >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              eos ut, consequatur incidunt reprehenderit repellendus ipsum
+              facilis. Dolor praesentium odit voluptatibus! Ducimus qui ratione
+              deleniti similique rem officia, officiis quibusdam?
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <div className="w-auto rounded-2xl">
+              <Image src={Imagen} alt="" className="rounded-2xl" />
+            </div>
+            <p className="font-bold spa my-2">Etica en la ingenieria</p>
+
+            <p className="my-2  text-xs flex items-center justify-center">18 de septiembre del 2022</p>
+
+            <p >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              eos ut, consequatur incidunt reprehenderit repellendus ipsum
+              facilis. Dolor praesentium odit voluptatibus! Ducimus qui ratione
+              deleniti similique rem officia, officiis quibusdam?
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <div className="w-auto rounded-2xl">
+              <Image src={Imagen} alt="" className="rounded-2xl" />
+            </div>
+            <p className="font-bold spa my-2">Etica en la ingenieria</p>
+
+            <p className="my-2  text-xs flex items-center justify-center">18 de septiembre del 2022</p>
+
+            <p >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              eos ut, consequatur incidunt reprehenderit repellendus ipsum
+              facilis. Dolor praesentium odit voluptatibus! Ducimus qui ratione
+              deleniti similique rem officia, officiis quibusdam?
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <div className="w-auto rounded-2xl">
+              <Image src={Imagen} alt="" className="rounded-2xl" />
+            </div>
+            <p className="font-bold spa my-2">Etica en la ingenieria</p>
+
+            <p className="my-2  text-xs flex items-center justify-center">18 de septiembre del 2022</p>
+
+            <p >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              eos ut, consequatur incidunt reprehenderit repellendus ipsum
+              facilis. Dolor praesentium odit voluptatibus! Ducimus qui ratione
+              deleniti similique rem officia, officiis quibusdam?
+            </p>
+          </div>
+
+
+          <div className="flex flex-col justify-center">
+            <div className="w-auto rounded-2xl">
+              <Image src={Imagen} alt="" className="rounded-2xl" />
+            </div>
+            <p className="font-bold spa my-2">Etica en la ingenieria</p>
+
+            <p className="my-2  text-xs flex items-center justify-center">18 de septiembre del 2022</p>
+
+            <p >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              eos ut, consequatur incidunt reprehenderit repellendus ipsum
+              facilis. Dolor praesentium odit voluptatibus! Ducimus qui ratione
+              deleniti similique rem officia, officiis quibusdam?
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <div className="w-auto rounded-2xl">
+              <Image src={Imagen} alt="" className="rounded-2xl" />
+            </div>
+            <p className="font-bold spa my-2">Etica en la ingenieria</p>
+
+            <p className="my-2  text-xs flex items-center justify-center">18 de septiembre del 2022</p>
+
+            <p >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              eos ut, consequatur incidunt reprehenderit repellendus ipsum
+              facilis. Dolor praesentium odit voluptatibus! Ducimus qui ratione
+              deleniti similique rem officia, officiis quibusdam?
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <div className="w-auto rounded-2xl">
+              <Image src={Imagen} alt="" className="rounded-2xl" />
+            </div>
+            <p className="font-bold spa my-2">Etica en la ingenieria</p>
+
+            <p className="my-2  text-xs flex items-center justify-center">18 de septiembre del 2022</p>
+
+            <p >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              eos ut, consequatur incidunt reprehenderit repellendus ipsum
+              facilis. Dolor praesentium odit voluptatibus! Ducimus qui ratione
+              deleniti similique rem officia, officiis quibusdam?
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <div className="w-auto rounded-2xl">
+              <Image src={Imagen} alt="" className="rounded-2xl" />
+            </div>
+            <p className="font-bold spa my-2">Etica en la ingenieria</p>
+
+            <p className="my-2  text-xs flex items-center justify-center">18 de septiembre del 2022</p>
+
+            <p >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              eos ut, consequatur incidunt reprehenderit repellendus ipsum
+              facilis. Dolor praesentium odit voluptatibus! Ducimus qui ratione
+              deleniti similique rem officia, officiis quibusdam?
+            </p>
+          </div>
+
+          
+        </section>
       </div>
     </main>
   );
